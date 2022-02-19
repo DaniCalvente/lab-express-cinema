176a6981b66681c25931a536ead288b3f0d62a84
Dani Calvente <danicalvente@hotmail.com>
2022-02-19 21:24:50 +0100

    iteraciones 1, 2, 3 hechas

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res, next) => {
+    // buscar nombres e imagenes de las peliculas
+    Movies.find()
+    .then((response) => {
+        console.log(response);
+        res.render("movies.hbs", {response})
+    })
+    .catch((err) => {
+
+    })
+
+    // mostrar una vista al cliente con los nombres y las imagenes
+})
+
+router.get("/movie/:id", (req, res, next) => {
+
+    Pokemon.findById( req.params.id)
+    .then((response) => {
+      console.log(response);
+      res.render("movie-details.hbs", {response})
+    })
+    .catch((err) => {
+      next(err)
+    })
+  })
 
 module.exports = router;
